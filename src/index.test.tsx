5720868061332c0154bc83bc82d14bd73f9a6d56
Rena <choco.rena0609@gmail.com>
2020-05-29 14:06:09 +0900

    テスト削除

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { generateHelloWorld, App } from "./app";
@@ -12,3 +12,5 @@ test("mounted text", () => {
   const { getByTestId } = render(<App></App>);
   expect(getByTestId("text")).toHaveTextContent("hello world.");
 });
+
+*/
