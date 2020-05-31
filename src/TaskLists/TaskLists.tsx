550cee623b1b8b9348ac9634493df0a196b3e2e8
Rena <choco.rena0609@gmail.com>
2020-05-31 11:17:18 +0900

    タスクの表示

--- src/TaskLists/TaskLists.tsx
+++ src/TaskLists/TaskLists.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { TasksQuery } from "./queries";
+import { tasks } from "../__generated__/types";
 
 export const TaskLists: React.FC = () => {
-  const { loading, error, data } = useQuery(TasksQuery);
+  const { loading, error, data } = useQuery<tasks>(TasksQuery);
   console.log(data);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
-  return <>Todo:タスクの表示</>;
-};
 
+  return (
+    <div>
+      <ul>
+        {data!.tasks.map((task) => (
+          <li key={task.id}>
+            <strong>{task.title}</strong>
+            <ul>
+              <li>{task.status}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
